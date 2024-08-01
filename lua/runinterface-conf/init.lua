c5099b6fa39797c6eeead10c176279c45119c8e5
Runinterface <divwa.nirou@gmail.com>
2024-08-01 23:06:22 +0300

    go

--- lua/runinterface-conf/init.lua
+++ lua/runinterface-conf/init.lua
@@ -1,5 +1,4 @@
 require("runinterface-conf.config.options")
 require("runinterface-conf.config.lazy")
-require("runinterface-conf.config.autocmd")
 require("runinterface-conf.config.keymaps")
-
+require('go').setup()
